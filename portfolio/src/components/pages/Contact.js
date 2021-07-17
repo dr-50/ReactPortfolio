88de279122a374bd826554eba2306cada85191f9
Devon <devonross50@yahoo.com>
2021-07-17 10:54:21 -0500

    basic contact page layout

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,7 +1,24 @@
-import React from 'react'
-function Contact(){
-    return (
-        <div>Contact Page</div>
-    )
+import React from "react";
+function Contact() {
+  return (
+    <section>
+      <h1>Contact Page</h1>
+      <div className="email-form">
+        <div className="email-name">
+            <label className='email-name-label'>Name: </label>
+            <input className='email-name-input'></input>
+        </div>
+        <div className="email">
+          <label className="email-label">Email: </label>
+          <input className="email-input"></input>
+        </div>
+        <div className="message">
+          <label className="message-label">Message: </label>
+          <textarea className="message-input"></textarea>
+        </div>
+        <button type="submit" className="messageSubmitBtn">Submit</button>
+      </div>
+    </section>
+  );
 }
 export default Contact;
